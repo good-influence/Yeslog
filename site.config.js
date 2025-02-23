e3cbfca7e4f1dfa229096e028e76a68a77ff0409
yesol <jjangye96@naver.com>
2025-02-23 22:06:54 +0900

    no message

--- site.config.js
+++ site.config.js
@@ -31,7 +31,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: "process.env.NOTION_PAGE_ID",
+    pageId: process.env.NOTION_PAGE_ID,
   },
 
   // plugin configuration (optional)
